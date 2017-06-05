92692fb8b026b87d03f87ebb0b9f32668797ad20
GersonPincheira <gerson.pincheira@gmail.com>
2017-06-05 17:09:51 -0400

    commit

--- camion.h
+++ camion.h
@@ -1,9 +1,15 @@
 #ifndef CAMION_H
 #define CAMION_H
-
-
+#include<cilindro.h>
+#include <deque>
+using namespace std;
 class Camion
 {
+private:
+    int tipo;
+    int capacidad;
+    bool reparto;
+    deque carga<Cilindro>;
 public:
     Camion();
 };
