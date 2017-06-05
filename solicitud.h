3b5e044404c790ddbc100e3897411b968b7bda8f
GersonPincheira <gerson.pincheira@gmail.com>
2017-06-05 00:42:11 -0400

    segundo commit

--- solicitud.h
+++ solicitud.h
@@ -1,11 +1,20 @@
 #ifndef SOLICITUD_H
 #define SOLICITUD_H
+#include <deque>
+#include <cilindro.h>
 
+using namespace std;
 
 class Solicitud
 {
 public:
     Solicitud();
+private:
+    deque pedidos<Cilindro>;
+    string ubicacion;
+    string MedioPago;
+    int TotalPago;
+    float TiempoDeEntrega;
 };
 
 #endif // SOLICITUD_H
